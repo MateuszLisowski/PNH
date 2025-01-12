48376b0d5e643809a92365607cf71713f89a9e6a
MateuszLisowski <mateuszlisowski@MacBook-Air-MateuszLisowski.local>
2025-01-12 17:43:32 +0100

    add home background

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,23 +3,26 @@ import styles from "./Home.module.css";
 
 export const Home = () => {
   return (
-    <main className={styles.container}>
-      <h1 className={styles.title}>
-        Tworzymy kulturę niezależną w Białymstoku
-      </h1>
-      <h2 className={styles.subtitle}>
-        Celem Projektu New Hope jest stworzenie przestrzeni, w której każda
-        osoba artystyczna, niezależnie od swojego doświadczenia, będzie mogła
-        rozwijać swoje pasje i umiejętności
-      </h2>
-      <div className={styles.buttons}>
-        <Link to="/offer" className={styles.linkButton}>
-          Warsztaty
-        </Link>
-        <Link to="/offer" className={styles.secondLink}>
-          Wydarzenia
-        </Link>
-      </div>
-    </main>
+    <>
+      <div className={styles.background}></div>
+      <main className={styles.container}>
+        <h1 className={styles.title}>
+          Tworzymy kulturę niezależną w Białymstoku
+        </h1>
+        <h2 className={styles.subtitle}>
+          Celem Projektu New Hope jest stworzenie przestrzeni, w której każda
+          osoba artystyczna, niezależnie od swojego doświadczenia, będzie mogła
+          rozwijać swoje pasje i umiejętności
+        </h2>
+        <div className={styles.buttons}>
+          <Link to="/offer" className={styles.linkButton}>
+            Warsztaty
+          </Link>
+          <Link to="/offer" className={styles.secondLink}>
+            Wydarzenia
+          </Link>
+        </div>
+      </main>
+    </>
   );
 };
