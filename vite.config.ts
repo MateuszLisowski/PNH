8f5719e33e4c50650b8026bfa97587c13d58ba2b
MateuszLisowski <mateuszlisowski@MacBook-Air-MateuszLisowski.local>
2025-01-20 22:47:24 +0100

    add changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "",
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "[name].[hash].[ext]", // Opcjonalnie, dla wersjonowania plików
+        chunkFileNames: "[name].[hash].js", // Opcjonalnie, dla wersjonowania plików
+      },
+    },
+  },
 });
